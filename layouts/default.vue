e1240cd9471690f478fd7f1e8685a38fa68a6a52
KeijiPlata <lordmiackykeijip@gmail.com>
2025-05-23 18:08:03 +0800

    chore: installation of nuxt and nuxt ui

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="min-h-screen w-full ">
+        <Navbar />
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>hello world hello</h1>
+    </div>
+</template>
